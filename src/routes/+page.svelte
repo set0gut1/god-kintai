14862a8a980003a8a027c8e677e26a64cb620502
Tsukasa Setoguchi <tsukasa.setoguchi@gmail.com>
2025-12-01 04:47:16 +0900

    コメント欄

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
   import dayjs from 'dayjs';
-  let currentTime = $state(dayjs('2025-12-01 09:59:40'));
+
+  let currentTime = $state(dayjs('2025-12-01 09:59:48'));
 
   type CurrentState = 'notyet' | 'working' | 'resting';
   let currentState: CurrentState = $state('notyet');
@@ -9,10 +11,32 @@
   const toResting = () => (currentState = 'resting');
   const toNotyet = () => (currentState = 'notyet');
 
-  $effect(() => {
+  type CommentType = 'normal' | 'system';
+
+  type Comment = {
+    text: string;
+    type: CommentType;
+  };
+
+  let commentArea;
+  const comments: Comment[] = $state([]);
+
+  const addComment = (text: string, type: CommentType, timeout: number) => {
+    setTimeout(() => {
+      comments.push({ text, type });
+      setTimeout(() => {
+        commentArea.scroll({ top: commentArea.scrollHeight, behavior: 'smooth' });
+      }, 0);
+    }, timeout);
+  };
+
+  onMount(() => {
     setInterval(() => {
       currentTime = currentTime.add(1, 's');
     }, 1000);
+    addComment('これ間に合う？', 'normal', 1000);
+    addComment('遅刻っぽ', 'normal', 1700);
+    addComment('ざわ・・・ざわ・・・', 'normal', 2900);
   });
 </script>
 
@@ -60,6 +84,12 @@
       </div>
       <p class="mt-10 text-gray-500">勤怠修正　各種申請　メモ</p>
     </div>
-    <div class="border-l">right</div>
+    <div class="border-l p-5 h-100 overflow-scroll" bind:this={commentArea}>
+      {#each comments as { text, type }}
+        <p class="border-b border-gray-400">
+          {text}
+        </p>
+      {/each}
+    </div>
   </div>
 </div>
